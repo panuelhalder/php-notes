c7fca587f04d6fd8fe30ffae815abb619b4334f2
panuelhalder <panuel.halder@yahoo.com>
2025-04-14 11:18:17 +0600

    add some vs code shortcut

--- first.php
+++ first.php
@@ -1,4 +1,23 @@
 <?php
-    echo "hello world form bangladesh"; //echo for display anything on browser;
+echo 
+"hello world...
+ form  bangladesh"; //echo for display anything on browser;
+
+    
+    /*********  Customized PHP keyboard shortcut ***************/
+    
+   //      ctrl + P and type @ in the search bar = FInd all symble and term 
+   //      ctrl + ~ = Open terminal
+   //      ctrl + D = Selected line duplicate
+   //      ctrl + P = Search by file name
+   //      ctrl + Tab = Move previous file
+   //      ctrl + 1/2/3 = Move to active VScode window
+   //      ctrl + shift + Up Arrow = increment numeric value
+   //      ctrl + shift + Down Arrow = Decrement numeric value
+   //      ctrl + shift + P = Open command pallet and search
+   //      ctrl + K ctrl + W = Close all active tab
+    
+    
+    
 
 ?>
