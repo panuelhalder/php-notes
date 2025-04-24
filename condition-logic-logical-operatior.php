ae3e741b191b2b731c08945657b19de3d4c8d497
panuelhalder <panuel.halder@yahoo.com>
2025-04-25 01:24:03 +0600

    new update

---
+++ condition-logic-logical-operatior.php
@@ -0,0 +1,103 @@
+<?php
+
+/*
+=   // assign
+==  // equal
+!=  // not equal
+>   // greater than
+<   // smaller than
+>=  // greater than equal to
+<=  // smaller than equal to
+*/
+
+$n = 13;
+
+if($n %2 == 0){
+    echo "$n is even number" . "\n";
+}else{
+    echo "$n is odd number" . "\n";
+};
+
+
+
+if($n > 10){
+    echo "$n is greater is than 10" . "\n";
+}else{
+    echo "$n is smaller than 10" . "\n";
+}
+
+
+/* if else condition er rules onujai, jodi 1ta condition true hoy tahole porer condition gula ar check hobe na; */
+
+$rahim = 400;
+$karim = 100;
+
+if($rahim == $karim){
+    echo "they have same amount";
+}elseif($rahim <= $karim){
+    echo "Rahim has smaller or equal amount than karim ";
+}else{
+    echo "They have different amount";
+}
+echo "\n";
+
+/*Uses of && with Age  */
+
+$age = 5;
+
+if($age >= 13 && $age <= 19){ // && = and
+    echo "He/she is a teen";
+}elseif($age < 13){
+    echo "He/she is a child";
+}else{
+    echo "He/she is an adult";
+}
+echo "\n";
+
+/*Uses of || Double pipe = Or. with Age  */
+
+$food = "Salmon";
+
+if($food == "tuna" || $food == "Salmon"){
+    echo "{$food} contains vitamin-D"; // variable {} er vitor use kore o dekhano jay "this" ba "it" na use kore
+}else {
+    echo "We don't know if {$food} contains vitamin-D";
+}
+
+echo "\n";
+
+
+/****** Leap Year Function   *******/
+
+/* Leap year rules
+
+// Divisible by 4
+// Not divisible by 100
+// Divisible by 400 (for century year)
+
+*/
+
+$year = "2024";
+
+if($year %4 == 0 && ($year %100 != 0 || $year %400 == 0)){
+    echo "{$year} is a leap year";
+}else{
+    echo "{$year} is not a leap year";
+}
+
+
+
+/*** Nested condition & logic ***/
+
+$username = "abcd";
+$password = "1234";
+
+if($username == "abcd"){
+    if($password == "1234"){
+        echo "Hello user";
+    }else {
+        echo "invalid password";
+    }
+}else {
+    echo "invalid username";
+}
--- printf.php
+++ printf.php
@@ -9,6 +9,8 @@ $firstName = "Isaac";
 $lastName = "Newton";
 printf("His fist name is %s, and last name is %s", strtoupper($firstName),strrev($lastName)); // printf e upper font support kore, echo te kore na
 
+
+
 echo "\n";
 
 printf('His name is %2$s, %1$s',$firstName, $lastName) ; //last name agey ar first name pore deyar jonno, ekhane "" use korle $ ke variable detect kore, tai '' use korte hobe
@@ -19,3 +21,34 @@ echo "\n";
 
 printf('The binary equipvalent of %1$d is %1$b', 12); //ekhane ekta argument diye 2 ta place holder er value deya jay
 
+echo "\n";
+
+
+/***   . er por 2 songkhar ghor nirnoy   ***/
+
+$n = 4.367;
+printf("%.2f",$n);
+
+echo "\n";
+
+/***   ongko bariye songkha print kora: 123 ke 00123 kora   ***/
+$m = 123;
+printf("%05d", $m); // 5 ongkey print kora
+echo "\n";
+
+
+/***   . er age 3 ghor ar . er pore 2 ghor   ***/
+$x = 123.567;
+$y = 23.789;
+
+printf("%07.2f",$x); // . soho total 7 ta ghor . er por 2 ta
+echo "\n";
+printf("%07.2f",$y); // . soho total 7 ta ghor . er por 2 ta
+echo "\n";
+
+
+/* sprintf = value ta print na kore variable er moddhe return kore ba joma rakhe */
+
+$output = sprintf("We are living on the %s", $name);
+
+echo $output; // ekhon return er man ta echo hoiche
